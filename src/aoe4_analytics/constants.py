f4337083d1d036308664f78acae00d73aadbf7c7
Oskar Olsson <o.olsson@outlook.com>
2025-01-18 13:49:20 +0100

    first commit

--- src/aoe4_analytics/constants.py
+++ src/aoe4_analytics/constants.py
@@ -1,1 +1,4 @@
 CIVS = ["abbasid", "japanese", "ayyubids", "byzantines", "chinese", "delhi", "english", "french", "hre", "jeannedarc", "malians", "mongols", "orderofthedragon", "ottomans", "rus", "zhuxi", "delhi"]
+
+GAMESDIR = "games"
+PLAYERDIR = "players"
